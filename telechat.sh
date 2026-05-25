55b81066a306f4841664e2ebefa6a4c0216339d1
Asinerum Conlang Project <46498542+asinerum@users.noreply.github.com>
2026-05-26 06:16:02 +0700

    Create telechat.sh

---
+++ telechat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export GEMINI_API_KEY=some_key
+export TELEGRAM_BOT_TOKEN=some_token
+export TELEGRAM_AUTH_USER_LIST=[id_list]
+export DOB=1/12/1981
+export TOB=15.00
+export TIME=week
+telepoo
